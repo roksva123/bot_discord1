15b3ad45cf9b89de30f12f6262d537bec3bff861
roksva123 <ikilua43@gmail.com>
2026-01-31 01:05:05 +0700

    feat: Update keep_alive port configuration, remove package.json, and add Dockerfile

--- keep_alive.py
+++ keep_alive.py
@@ -9,7 +9,7 @@ def home():
     return "Bot is alive!"
 
 def run():
-  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+  app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 7860)))
 
 def keep_alive():
     t = Thread(target=run)
